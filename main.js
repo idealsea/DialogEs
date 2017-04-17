0e3b10a6b58d74c0f555d96f00834bd156206333
Haitao <ideal_sea@qq.com>
2017-04-17 14:39:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+//Dialog有两个参数，第二个参数可选，第一个参数是提示框内容。
+//默认对话框没有取消按钮，在第二个参数里把对象的cancelShow属性设置成true就可以显示。
+//第二个参数示例
+//    var option = {
+//        ok:function () {
+//            console.log('我是确定')
+//        },
+//        cancel:function () {
+//            console.log('我是取消')
+//        },
+//        cancelShow: false //是否显示取消按钮
+//    };
+//    var dialog1 = new DialogEs('用户名密码错误！');
+
 function DialogEs(content,obj) {
         if(obj === null || typeof obj !== 'object'){
             obj = {};
